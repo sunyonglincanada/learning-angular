15310cbc4c90770cf8d09adea30692a2b5b41044
Eric Sun <eric.sun@vip.51.ca>
2018-10-31 12:42:52 -0400

    ADD: add list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../classes/hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,8 +14,14 @@ export class HeroesComponent implements OnInit {
       name: 'Windstorm'
     };
 
+    heroes = HEROES;
+    selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() { }
 
+  onSelect(hero: Hero): void {
+     this.selectedHero = hero;
+  }
 }
